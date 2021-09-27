cc0def54538db6c5f5f684238ebe120834585c8e
Tahani <too0to0@hotmail.com>
2021-09-27 15:18:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewapp/MessageAdapter.kt
@@ -0,0 +1,32 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewapp.R
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class MessageAdapter(val context: Context, val messages: ArrayList<String>):
+    RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+    class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        return MessageViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = messages[position]
+
+        holder.itemView.apply {
+            tvColor.text = message
+        }
+    }
+    override fun getItemCount() = messages.size
+}
